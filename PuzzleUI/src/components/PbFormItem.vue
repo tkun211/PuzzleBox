73df57a4dd72ef4cb175066d68e8987605386c83
tkun <tkun@aliyun.com>
2024-04-28 23:08:46 +0800

    表单一个简单的提交

---
+++ PuzzleUI/src/components/PbFormItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="pb-form-item">
+    <label
+      :style="{
+        width: labelWidth || labelWidth === 0 ? `${labelWidth}px` : 'auto',
+      }"
+      >{{ label }}</label
+    >
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: { type: String, default: () => '' },
+  labelWidth: { type: Number },
+});
+</script>
+
+<style scoped>
+.pb-form-item {
+  display: flex;
+}
+
+label {
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 1;
+  justify-content: center;
+  overflow: hidden;
+  line-height: 1.2;
+}
+</style>
